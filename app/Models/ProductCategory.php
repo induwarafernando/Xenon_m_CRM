e75b75b5d565c72715fcff507009c00fe0fdcd0e
induwarafernando <induwarafernando9@gmail.com>
2024-01-11 14:50:47 +0530

    CRUD commit 1

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'parent_id',
+        'status',
+        'image',
+        'meta_title',
+        'meta_description',
+        'meta_keywords',
+        
+    ];  
+
+
+}
