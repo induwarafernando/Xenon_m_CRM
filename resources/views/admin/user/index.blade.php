0b44952130d479b3dbf266c0554e081eb5a4685d
induwarafernando <induwarafernando9@gmail.com>
2024-01-30 09:58:33 +0530

    register

---
+++ resources/views/admin/user/index.blade.php
@@ -0,0 +1,108 @@
+<x-app-layout>
+
+    <div class="container mx-auto mt-1">
+        <div class="px-4 sm:px-6 lg:px-8 bg-white pt-4">
+
+            @if (session('success'))
+                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mt-4 mb-5"
+                    role="alert">
+                    <strong class="font-bold">Success!</strong>
+                    <span class="block sm:inline">{{ session('success') }}</span>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20">
+                            <title>Close</title>
+                            <path
+                                d="M14.348 5.652a.5.5 0 010 .707L9.707 10l4.641 4.641a.5.5 0 11-.707.707L9 10.707l-4.641 4.641a.5.5 0 11-.707-.707L8.293 10 3.652 5.359a.5.5 0 01.707-.707L9 9.293l4.641-4.641a.5.5 0 01.707 0z"
+                                clip-rule="evenodd" fill-rule="evenodd"></path>
+                        </svg>
+                    </span>
+                </div>
+            @endif
+
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-base font-semibold leading-6 text-gray-900">Users</h1>
+                    <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name,
+                        title, email and role.</p>
+                </div>
+                <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <a href="{{ route('user.create') }}"
+                        class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Create User
+                    </a>
+                </div>
+            </div>
+
+
+            <div class="mt-8 flow-root">
+                <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead>
+                                <tr>
+                                    <th scope="col"
+                                        class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                        ID
+                                    </th>
+                                    <th scope="col"
+                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Name</th>
+                                    <th scope="col"
+                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Email</th>
+                                    <th scope="col"
+                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Role</th>
+                                    <th scope="col"
+                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Actions</th>
+                                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                                        <span class="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200">
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td
+                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            {{ $user->id }}</td>
+                                        <td
+                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            {{ $user->name }}</td>
+                                        <td
+                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            {{ $user->email }}</td>
+                                        <td
+                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            {{ ucwords(str_replace('_', ' ', Str::snake($user->role->name))) }}</td>
+                                        <td
+                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            <div class="flex gap-3">
+                                                <a href="{{ route('user.show', $user->id) }}"
+                                                    class="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Show</a>
+                                                <a href="{{ route('user.edit', $user->id) }}"
+                                                    class="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Edit</a>
+                                                <form
+                                                    action="{{ route('user.destroy', $user->id) }}"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Delete</button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{ $users->links() }}
+    </div>
+</x-app-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
@@ -59,4 +59,87 @@
     </x-authentication-card>
 </x-guest-layout>
 
+ --}}
+
+ <x-guest-layout>
+    <div class="bg-no-repeat bg-cover bg-top relative" style="background-image: url(https://i.ibb.co/Q70q5f8/wepik-export-20240123073209-YAqa.jpg); height:695px">
+        <div class="absolute bg-gradient-to-b from-blue-500 to-blue-900 opacity-60 inset-0 z-0"></div>
+        <div class="min-h-screen sm:flex sm:flex-row mx-0 justify-center">
+            <div class="flex justify-center self-center z-10">
+                 <!-- Left side with branding -->
+                   <div class="self-start hidden lg:flex flex-col text-white mr-32">
+                      <img src="" class="mb-3">
+                      <h1 class="mb-3 mt-64 font-bold text-5xl">Hi! Welcome Back</h1>
+                      <p class="pr-3">"Your Closet's Best Friend – XENON, Your Ultimate Delivery Partner."</p>
+                   </div>
+                   
+                   <!-- Authentication Card -->
+                   <div class="flex justify-center self-center z-10">
+                        <div class="p-12 bg-white mx-auto rounded-2xl w-100 ">
+                          <x-validation-errors class="mb-4" />
+                          
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                        <div class="mb-4">
+                            <div class="flex justify-left items-left">
+                                <div class="relative">
+                                  <div class="ml-2 mt-5 w-8 h-8 bg-gradient-to-br from-blue-400  to-green-500 rounded-full animate-pulse absolute top-1/12 left-1/12 transform -translate-x-1/2 -translate-y-1/2 opacity-15" ></div>
+                                  <div class="ml-2 mt-5 w-6 h-6 bg-gradient-to-br from-blue-400  to-green-500 rounded-full animate-pulse absolute top-1/64 left-1/64 transform -translate-x-1/2 -translate-y-1/2 opacity-30"></div>
+                                  <div class="ml-2 mt-5 w-4 h-4 bg-gradient-to-br from-blue-400  to-green-500 rounded-full animate-pulse absolute top-1/128 left-1/128 transform -translate-x-1/2 -translate-y-1/2 opacity-40"></div>
+                                  <div class="ml-2 mt-5 w-3 h-3 bg-gradient-to-br from-blue-400  to-green-500 rounded-full animate-pulse absolute top-1/256 left-1/256 transform -translate-x-1/2 -translate-y-1/2"></div>
+                                </div> 
+                            </div>    
+                            <h3 class="font-bold text-3xl text-gray-800 pl-8 pt-1">Register</h3>
+                            <p class="text-gray-500">Create your account to get started.</p>
+                        </div>
+                        <div class="space-y-5">
+                            <div class="space-y-2">
+                                <label class="text-sm font-medium text-gray-700 tracking-wide">Name</label>
+                                <x-input id="name" class="w-full text-base px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-green-400" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Your Name" />
+                            </div>
+                            <div class="space-y-2">
+                                <label class="text-sm font-medium text-gray-700 tracking-wide">Email</label>
+                                <x-input id="email" class="w-full text-base px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-green-400" type="email" name="email" :value="old('email')" required autocomplete="username" placeholder="mail@gmail.com" />
+                            </div>
+                            <div class="space-y-2">
+                                <label class="text-sm font-medium text-gray-700 tracking-wide">Password</label>
+                                <x-input id="password" class="w-full content-center text-base px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-green-400" type="password" name="password" required autocomplete="new-password" placeholder="Enter your password" />
+                            </div>
+                            <div class="space-y-2">
+                                <label class="mb-5 text-sm font-medium text-gray-700 tracking-wide">Confirm Password</label>
+                                <x-input id="password_confirmation" class="w-full content-center text-base px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-green-400" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm your password" />
+                            </div>
+                            @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
+                                <div class="mt-4">
+                                    <x-label for="terms">
+                                        <div class="flex items-center">
+                                            <x-checkbox name="terms" id="terms" required />
+                                            <div class="ms-2">
+                                                {!! __('I agree to the :terms_of_service and :privacy_policy', [
+                                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Terms of Service').'</a>',
+                                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Privacy Policy').'</a>',
+                                                ]) !!}
+                                            </div>
+                                        </div>
+                                    </x-label>
+                                </div>
+                            @endif
+                            <div>
+                                <x-button class="w-full flex justify-center bg-blue-500 hover:bg-blue-700 text-gray-100 p-3 rounded-full tracking-wide font-bold shadow-lg cursor-pointer transition ease-in duration-500">
+                                    Register
+                                </x-button>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="pt-5 text-center text-gray-400 text-xs">
+                        <span>
+                            Copyright © 2021-2022
+                            <a href="https://codepen.io/uidesignhub" rel="" target="_blank" title="Ajimon" class="text-green hover:text-green-500">AJI</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
 
