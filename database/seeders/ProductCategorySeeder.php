0f6a10841abade68cf807d3d552c0e885a1b2ab2
induwarafernando <induwarafernando9@gmail.com>
2024-02-08 00:30:46 +0530

    expand data

--- database/seeders/ProductCategorySeeder.php
+++ database/seeders/ProductCategorySeeder.php
@@ -24,6 +24,25 @@ class ProductCategorySeeder extends Seeder
             ['name' => 'Nightdress', 'slug' => 'nightdress', 'description' => 'Comfortable Nightwear'],
             ['name' => 'Shirts', 'slug' => 'shirts', 'description' => 'Versatile Wardrobe Staple'],
             ['name' => 'T-Shirts Oversized', 'slug' => 't-shirts-oversized', 'description' => 'Relaxed Casual Wear'],
+            ['name' => 'T-Shirts Regular', 'slug' => 't-shirts-regular', 'description' => 'Everyday Essential'],
+            ['name' => 'Trousers', 'slug' => 'trousers', 'description' => 'Tailored and Polished'],
+            ['name' => 'Shorts', 'slug' => 'shorts', 'description' => 'Casual and Comfortable'],
+            ['name' => 'Skirts', 'slug' => 'skirts', 'description' => 'Feminine and Flirty'],
+            ['name' => 'Sweaters', 'slug' => 'sweaters', 'description' => 'Cozy and Warm'],
+            ['name' => 'Jackets', 'slug' => 'jackets', 'description' => 'Stylish Outerwear'],
+            ['name' => 'Blazers', 'slug' => 'blazers', 'description' => 'Sharp and Sophisticated'],
+            ['name' => 'Coats', 'slug' => 'coats', 'description' => 'Elegant Winter Wear'],
+            ['name' => 'Hoodies', 'slug' => 'hoodies', 'description' => 'Casual and Comfortable'],
+            ['name' => 'Sweatshirts', 'slug' => 'sweatshirts', 'description' => 'Relaxed and Sporty'],
+            ['name' => 'Dresses', 'slug' => 'dresses', 'description' => 'Elegant and Feminine'],
+            ['name' => 'Tops', 'slug' => 'tops', 'description' => 'Versatile and Stylish'],
+            ['name' => 'Jeans', 'slug' => 'jeans', 'description' => 'Classic and Comfortable'],
+            ['name' => 'Lingerie', 'slug' => 'lingerie', 'description' => 'Comfortable and Stylish'],
+            ['name' => 'Activewear', 'slug' => 'activewear', 'description' => 'Stylish and Functional'],
+        
+            
+
+
         ];
 
         // Insert each category into the database
