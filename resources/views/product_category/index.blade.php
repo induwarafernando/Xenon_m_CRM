560be7664ba9c4d30d131488f973a7bd2eec39b1
induwarafernando <induwarafernando9@gmail.com>
2024-02-09 12:38:50 +0530

    colour

--- resources/views/product_category/index.blade.php
+++ resources/views/product_category/index.blade.php
@@ -29,7 +29,7 @@
                 </div>
                 <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                     <a href="{{ route('product_category.create') }}"
-                        class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        class="block rounded-md bg-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                         Create Category
                     </a>
                 </div>
@@ -37,41 +37,38 @@
             <div class="mt-8 flow-root">
                 <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                        <table class="min-w-full divide-y divide-gray-300">
-                            <thead>
+                        <table class="w-full text-left divide-y divide-gray-300 rounded-lg overflow-hidden">
+                            <thead class="text-xs text-white uppercase bg-blue-700 border-b border-blue-400 dark:text-white">
                                 <tr>
                                     <th scope="col"
-                                        class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                        class="px-6 py-3">
                                         ID
                                     </th>
                                     <th scope="col"
-                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        class="px-6 py-3">
                                         Name</th>
                                     <th scope="col"
-                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        class="px-6 py-3">
                                         Description</th>
                                     <th scope="col"
-                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        class="px-6 py-3">
                                         Actions</th>
-                                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                                        <span class="sr-only">Edit</span>
-                                    </th>
                                 </tr>
                             </thead>
                             <tbody class="divide-y divide-gray-200">
-                                @foreach ($product_categories as $product_category)
-                                    <tr>
+                                @foreach ($product_categories as $index => $product_category)
+                                    <tr class="{{ $index % 2 == 0 ? 'bg-blue-100' : 'bg-blue-50' }} border-b border-blue-400 text-black hover:bg-blue-200">
                                         <td
-                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                             {{ $product_category->id }}</td>
                                         <td
-                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                             {{ $product_category->name }}</td>
                                         <td
-                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                             {{ $product_category->description }}</td>
                                         <td
-                                            class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                                             <div class="flex gap-3">
                                                 <a href="{{ route('product_category.show', $product_category->id) }}"
                                                     class="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Show</a>
@@ -88,7 +85,6 @@
                                         </td>
                                     </tr>
                                 @endforeach
-
                             </tbody>
                         </table>
                     </div>
