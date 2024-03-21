9d16bee2f23074e144754678f741d905cf230338
induwarafernando <induwarafernando9@gmail.com>
2024-03-21 09:25:21 +0530

    cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,21 +2,27 @@
 
 namespace App\Models;
 
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-    class Cart extends Model {
-        protected $fillable = [
+class Cart extends Model
+{
+    protected $fillable = [
         'item_count',
         'sub_total',
         'total_discount',
         'total',
         'total_tax',
         'is_paid',
-        ];
+    ];
 
-        protected function user(): BelongsTo
-        {
+    protected function user(): BelongsTo
+    {
         return $this->belongsTo(User::class);
-        }
     }
+
+    protected function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
--- database/migrations/2024_03_21_091323_create_carts_table.php
+++ database/migrations/2024_03_21_091323_create_carts_table.php
@@ -4,20 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-$table->foreignId('user_id');
-$table->smallInteger('item_count');
-$table->float('sub_total');
-$table->float('total_discount');
-$table->float('total');
-$table->float('total_tax')->nullable();
-$table->boolean('is_paid');
-$table->timestamps();//
+            $table->foreignId('user_id');
+            $table->smallInteger('item_count');
+            $table->float('sub_total')->default(0);
+            $table->float('total_discount')->default(0);
+            $table->float('total')->default(0);
+            $table->float('total_tax')->default(0)->nullable();
+            $table->boolean('is_paid')->default(false);
+            $table->timestamps();
         });
     }
 
