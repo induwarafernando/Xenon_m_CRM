d2eff33e7ce650074bccf2c64c48e0352878d1c6
induwarafernando <induwarafernando9@gmail.com>
2024-05-30 09:42:54 +0530

    dashboard nav bar

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -33,7 +33,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('products.create') }}" :active="request()->routeIs('products.create')">
-                        {{ __('Products') }}
+                        {{ __('Add Products') }}
                     </x-nav-link>
                 </div>
 
